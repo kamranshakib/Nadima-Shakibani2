f630aa51f1397331dd80ebf5e23e35b6ea0ce306
Kamran Shakib <kamranshakib371@gmail.com>
2025-01-02 10:40:02 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const sun = document.getElementById('sun');
+const body_main = document.getElementById('body-main');
+const container =  document.querySelector(".container");
+
+
+// const sun = document.getElementById('sun')
+// const sun = document.getElementById('sun')
+// const sun = document.getElementById('sun')
+
+sun.addEventListener('click',function(){
+    // console.log("hello for every one ")
+    body_main.style.backgroundColor='white'
+    body_main.style.color='black'
+    
+
+   
+})
